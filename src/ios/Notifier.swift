4860a8b7be26d1f09beb5a61a8263552f71a26d5
AhmedAyachi <60969587+AhmedAyachi@users.noreply.github.com>
2022-04-02 16:54:46 +0100

    ios:add toast method

--- src/ios/Notifier.swift
+++ src/ios/Notifier.swift
@@ -2,4 +2,20 @@
 
 class Notifier:NotifierPlugin{
     
+    @objc(toast:)
+    func toast(command:CDVInvokedUrlCommand){
+        let argument=command.arguments[0] as? [AnyHashable:Any];
+        if !(argument==nil){
+            let props=argument!;
+            let text=props["text"] as? String;
+            let lasting=(props["lasting"] as? String) ?? "short";
+            let duration=lasting=="long" ? 3.5:2;
+            let alert=UIAlertController(title:"",message:text,preferredStyle:.actionSheet);
+            DispatchQueue.main.asyncAfter(deadline:DispatchTime.now()+duration){
+                alert.dismiss(animated:true);
+            }
+            self.viewController.present(alert,animated:true);
+        }
+    }
+
 }
